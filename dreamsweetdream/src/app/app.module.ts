d39c13659db707a720748cfed0322a0c8706ed98
Lucas Nascimento <csalucasnascimento@gmail.com>
2017-12-13 14:48:06 +1100

    reset, pallete, theme and material components

--- dreamsweetdream/src/app/app.module.ts
+++ dreamsweetdream/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialComponentsModule } from './material-components.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
